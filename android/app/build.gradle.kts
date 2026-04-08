1c1f1d64f6d68e2968240ff139ca974200835647
Velora <velorastream@gmail.com>
2026-04-08 22:36:33 +0530

    chore: bump version to 1.9.0 (versionCode 29) - Premium Offers update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.cinevault.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 28
-        versionName = "1.8.0"
+        versionCode = 29
+        versionName = "1.9.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables {
